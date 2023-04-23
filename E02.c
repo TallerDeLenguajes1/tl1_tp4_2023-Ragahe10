dbc76d8323eb303583c3b1ab55b73b76e39ed9d6
RAMIRO <ragahe@gmail.com>
2023-04-23 15:01:12 -0300

    Tp05 finalizado

--- E02.c
+++ E02.c
@@ -24,33 +24,155 @@ void mostrarTareas(Lista L);
 void descripcion(char **k);
 void liberarLista(Lista *L);
 int longitud (Lista L);
-Lista quitarNodo(Lista *L);
+Lista sacarNodo(Lista *L);
 Lista controlarTareas(Lista L1, Lista *L2);
 Lista buscarTareaPorPalabra(Lista Lp, Lista Lr);
-Lista busquedaTareaPorID(Lista Lp, Lista Lr);
+Lista busquedaTareaPorID(Lista Lp, int id);
+void eliminarTarea(struct Nodo* Nodo);
+struct Nodo* quitarNodoPorID(Lista *L , int id);
+void insertarNodo(Lista *L, struct Nodo* nodo);
 
 int main ()
 {
     srand(time(NULL));
-    Lista TPendientes, TRealizadas, nod;
+    Lista TPendientes, TRealizadas, TProceso, nod;
     TPendientes = crearLista();
     TRealizadas = crearLista();
+    TProceso = crearLista();
     cargarTareas(&TPendientes);
-    mostrarTareas(TPendientes);
+    // eliminarTarea(quitarNodoPorID(&TPendientes, 2));
+    // mostrarTareas(TPendientes);
     
-    TPendientes = controlarTareas(TPendientes, &TRealizadas);
-    puts("-----TAREAS PENDIENTES------");
-    mostrarTareas(TPendientes);
-    puts("-----TAREAS REALIZADAS------");
-    mostrarTareas(TRealizadas);
+    // TPendientes = controlarTareas(TPendientes, &TRealizadas);
+    // puts("-----TAREAS PENDIENTES------");
+    // mostrarTareas(TPendientes);
+    // puts("-----TAREAS REALIZADAS------");
+    // mostrarTareas(TRealizadas);
    
-    nod = busquedaTareaPorID(TPendientes,TRealizadas);
-    mostrarTarea(&nod->T);
-    nod = buscarTareaPorPalabra(TPendientes,TRealizadas);
-    mostrarTarea(&nod->T);
+    // nod = busquedaTareaPorID(TPendientes,TRealizadas);
+    // mostrarTarea(&nod->T);
+    // nod = buscarTareaPorPalabra(TPendientes,TRealizadas);
+    // mostrarTarea(&nod->T);
+
+    // Listar Tareas
+    puts("-----TAREAS PENDIENTES-----");
+    mostrarTareas(TPendientes);
+    puts("-----TAREAS EN PROCESO-----");
+    mostrarTareas(TProceso);
+    puts("-----TAREAS REALIZADAS-----");
+    mostrarTareas(TRealizadas);
+
+    int resp, id, list;
+    puts("Desea mover alguna tarea?");
+    puts("0- No.");
+    puts("1- Si.");
+    printf("RESPUESTA: ");
+    scanf("%d", &resp);
+    fflush(stdin);
+    while (resp == 1){
+        printf("ID de la tarea a mover: ");
+        scanf("%d", &id);
+        if(busquedaTareaPorID(TPendientes, id)){
+            list = 1;
+        }else{
+            if(busquedaTareaPorID(TProceso, id)){
+                list = 2;
+            }else{
+                if (busquedaTareaPorID(TRealizadas, id)) {
+                    list = 3;
+                }else{
+                    list = 0;
+                }  
+            }
+        }
+        switch (list){
+            case 1:
+                printf("A donde desea mover?\n");
+                printf("- No mover (0)\n");
+                printf("- Tareas en Proceso (1)\n");
+                printf("- Tareas Realizadas (2)\n");
+                scanf("%d", &resp);
+                fflush(stdin);
+                switch (resp){
+                    case 1:
+                        nod = quitarNodoPorID(&TPendientes,id);
+                        insertarNodo(&TProceso, nod);
+                        break;
+                    case 2:
+                        nod = quitarNodoPorID(&TPendientes,id);
+                        insertarNodo(&TRealizadas, nod);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 2:
+                printf("A donde desea mover?\n");
+                printf("- No mover (0)\n");
+                printf("- Tareas Pendientes (1)\n");
+                printf("- Tareas Realizadas (2)\n");
+                scanf("%d", &resp);
+                fflush(stdin);
+                switch (resp){
+                    case 1:
+                        nod = quitarNodoPorID(&TProceso,id);
+                        insertarNodo(&TPendientes, nod);
+                        break;
+                    case 2:
+                        nod = quitarNodoPorID(&TProceso,id);
+                        insertarNodo(&TRealizadas, nod);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 3:
+                printf("A donde desea mover?\n");
+                printf("- No mover (0)\n");
+                printf("- Tareas Pendientes (1)\n");
+                printf("- Tareas en Proceso (2)\n");
+                scanf("%d", &resp);
+                fflush(stdin);
+                switch (resp){
+                    case 1:
+                        nod = quitarNodoPorID(&TRealizadas,id);
+                        insertarNodo(&TPendientes, nod);
+                        break;
+                    case 2:
+                        nod = quitarNodoPorID(&TRealizadas,id);
+                        insertarNodo(&TProceso, nod);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                puts("No existe la tarea");
+                break;
+        }
+        puts("-----TAREAS PENDIENTES-----");
+        mostrarTareas(TPendientes);
+        puts("-----TAREAS EN PROCESO-----");
+        mostrarTareas(TProceso);
+        puts("-----TAREAS REALIZADAS-----");
+        mostrarTareas(TRealizadas);
+        puts("Desea mover otra tarea?");
+        puts("0- No.");
+        puts("1- Si.");
+        printf("RESPUESTA: ");
+        scanf("%d", &resp);
+        fflush(stdin);
+    }
+    puts("-----TAREAS PENDIENTES-----");
+    mostrarTareas(TPendientes);
+    puts("-----TAREAS EN PROCESO-----");
+    mostrarTareas(TProceso);
+    puts("-----TAREAS REALIZADAS-----");
+    mostrarTareas(TRealizadas);
 
     liberarLista(&TPendientes);
     liberarLista(&TRealizadas);
+    liberarLista(&TProceso);
     return 0;
 }
 
@@ -66,6 +188,7 @@ struct Nodo* crearNodo(int id)
     nodo->T.TareaID = id;
     nodo->T.Duracion = 10 + rand()%91;
     descripcion(&nodo->T.Descripcion);
+    nodo->Siguiente = NULL;
 
     return nodo;
 }
@@ -102,10 +225,16 @@ void mostrarTarea(Tarea* T)
 
 void mostrarTareas(Lista L)
 {
+    int suma = 0;
     while (L != NULL){
        mostrarTarea(&L->T);
+       suma = suma + L->T.Duracion;
        L = L->Siguiente;
     }
+    if(L=NULL){
+        puts("--NO HAY TAREAS--");
+    }
+    printf("\n--TIEMPO TOTAL: %d--\n", suma);
 }
 
 void descripcion(char **k)
@@ -128,7 +257,7 @@ void liberarLista(Lista *L)
     }
 }
 
-Lista quitarNodo(Lista *L)
+Lista sacarNodo(Lista *L)
 {
     struct Nodo * nodo = NULL;
     if((*L) != NULL){
@@ -147,6 +276,7 @@ int longitud (Lista L)
     }
     return cant;
 }
+
 Lista controlarTareas(Lista L1, Lista *L2)
 {
     Lista aux, LP;
@@ -155,15 +285,18 @@ Lista controlarTareas(Lista L1, Lista *L2)
     puts("--------CONTROLAR TAREAS--------");
     while (L1 != NULL){
         mostrarTarea(&(L1->T));
-        printf("\nRealizo la tarea? (Si:1/No:0)");
+        printf("\nA donde desea mover?");
+        printf("\n- No mover (0)");
+        printf("\n- A 'En Proceso' (1)");
+        printf("\n- A 'Realizadas' (2)");
         scanf("%d", &resp);
         fflush(stdin);
-        if (resp == 0){
-            aux = quitarNodo(&L1);
+        if (resp == 2){
+            aux = sacarNodo(&L1);
             aux->Siguiente = LP;
             LP = aux;
         }else{
-            aux = quitarNodo(&L1);
+            aux = sacarNodo(&L1);
             aux->Siguiente = (*L2);
             (*L2) = aux;
         }
@@ -194,25 +327,53 @@ Lista buscarTareaPorPalabra(Lista Lp, Lista Lr)
     return NULL;
 }
 
-Lista busquedaTareaPorID(Lista Lp, Lista Lr)
+Lista busquedaTareaPorID(Lista Lp, int id)
 {
-    int id;
-    printf("\nIngrese el ID de la tarea a buscar");
-    scanf("%d", &id);
-    fflush(stdin);
     while (Lp){
         if(Lp->T.TareaID == id){
-            puts("Esta en PENDIENTES");
             return(Lp);
         }
         Lp = Lp->Siguiente;
-    }
-    while (Lr){
-        if(Lr->T.TareaID == id){
-            puts("Esta en REALIZADAS");
-            return(Lr);
-        }
-        Lr=Lr->Siguiente;
     }   
     return NULL;
+}
+
+void eliminarTarea(struct Nodo* Nodo)
+{
+    free(Nodo->T.Descripcion);
+    free(Nodo);
+}
+
+struct Nodo* quitarNodoPorID(Lista *L , int id)
+{
+    struct Nodo *aux, *auxAnterior;
+
+    if(*L != NULL){
+        if((*L)->Siguiente ==NULL){
+           if( (*L)->T.TareaID == id){ 
+                aux = *L;
+                *L = NULL;
+                return aux;
+           }
+        }else{
+            auxAnterior = (*L);
+            aux = (*L)->Siguiente;
+            while(aux->Siguiente != NULL && aux->T.TareaID !=id){
+                auxAnterior = aux;
+                aux = aux->Siguiente;
+            }
+            if(aux->T.TareaID == id){
+                auxAnterior->Siguiente = aux->Siguiente;
+                return aux;
+            }
+
+        }
+    }
+    return NULL;
+}
+
+void insertarNodo(Lista *L, struct Nodo* nodo)
+{
+    nodo->Siguiente = (*L);
+    (*L) = nodo;
 }
